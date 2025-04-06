d415e65ddab8790e05fd52ff2527d7b43f64cea9
Glitch (glitch-hello-website) <none>
2025-04-06 12:50:52 +0000

    🎳🦄 Checkpoint ./component.html:77024800/227 ./component.html copy:77024800/5128 ./style.css:77024800/947 ./script.js:77024800/100

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
   document.activeElement.blur();
 }
+
+document.addEventListener('touchmove', function(e) {
+  e.preventDefault();
+}, { passive: false });
