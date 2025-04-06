28eae9bfc43d8b1fbe44736e3bc0a151f4070d0d
Glitch (glitch-hello-website) <none>
2025-04-06 13:25:09 +0000

    📯📖 Checkpoint ./style.css:77024800/8 ./script.js:77024800/251

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
 document.addEventListener('touchmove', function(e) {
   e.preventDefault();
 }, { passive: false });
+
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
